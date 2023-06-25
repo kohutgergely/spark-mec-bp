884473bd7bdddc926ac2b6ab8870a68d6c209f4d
Gergely Kohut <kohut.gergely@gmail.com>
2023-06-25 22:41:38 +0200

    continues refactor

--- new_codebase/MEBS_for_AuAg.py
+++ new_codebase/MEBS_for_AuAg.py
@@ -19,9 +19,7 @@ def peakfinder(Y, height):
 def integral(spectrum, peaks):
     X = spectrum[:, 0]
     Y = spectrum[:, 1]
-    prominences = peak_prominences(Y, peaks, wlen=set_wlen)
-    left_base = prominences[1]
-    right_base = prominences[2]
+    _, left_base, right_base = peak_prominences(Y, peaks, wlen=set_wlen)
     integrals = np.zeros((X.size, 2))
     for i in range(0, left_base.size):
         peak_X = X[left_base[i]:right_base[i]:1]
@@ -60,8 +58,7 @@ def Voigt_integral(spectrum, peaks, selected_peak):
 
 def find_nearest(array, value):
     array = np.asarray(array)
-    idx = (np.abs(array - value)).argmin()
-    return idx
+    return (np.abs(array - value)).argmin()
 
 
 def spec_data(species, peak_table, spectrum, integrals, peaks):
@@ -113,45 +110,21 @@ def line_pair(peak_table, line_data, T):
 
     return linepairs
 
-## Saha-Boltzmann line-pair plots for Ag/Au###
+
+def fit_graph(graph):
+    return np.polyfit(graph[:, 0], graph[:, 1], 1)
 
 
-def AuAg_n_concentration(AuAg_graph):
-    AuAg_fit = np.polyfit(AuAg_graph[:, 0], AuAg_graph[:, 1], 1)
-    TAuAg = (1/(0.695035*AuAg_fit[0]))
-    partition_function_ag1 = get_partition_function("Ag I", TAuAg)
-    partition_function_ag2 = get_partition_function("Ag II", TAuAg)
-    partition_function_au1 = get_partition_function("Au I", TAuAg)
-    partition_function_au2 = get_partition_function("Au II", TAuAg)
-    partition_function_ar1 = get_partition_function("Ar I", TAuAg)
-    partition_function_ar2 = get_partition_function("Ar II", TAuAg)
-    first_ionization_energy_ag = get_ionization_energy("Ag I")
-    first_ionization_energy_au = get_ionization_energy("Au I")
-    first_ionization_energy_ar = get_ionization_energy("Ar I")
-    nAuAg = np.exp(AuAg_fit[1])*(partition_function_au1/partition_function_ag1)
-    # n_ion_AuAg = nAuAg*(ZII(TAuAg, Au)/ZII(TAuAg, Ag))*(ZI(TAuAg, Ag)/ZI(TAuAg, Au))*np.exp((Ionization_Ag[0]-Ionization_Au[0])/(0.695035*TAuAg))
+def caculate_temperature(fitted_graph):
+    return 1/(0.695035*fitted_graph[0])
 
-    ne = estimate_electron_concentration(
-        TAuAg, first_ionization_energy_ar, partition_function_ar1, partition_function_ar2)
-    nAgion_atom = SB(
-        ne, TAuAg, first_ionization_energy_ag)*(partition_function_ag2/partition_function_ag1)
-    nAuion_atom = SB(
-        ne, TAuAg, first_ionization_energy_au)*(partition_function_au2/partition_function_au1)
-    # nAgatom_ion = 1/nAgion_atom
 
-    total_n_AuAg = ((nAuion_atom+1)/(nAgion_atom+1))*nAuAg
+def calculate_nAuAg(AuAg_fit, partition_function_au1, partition_function_ag1):
+    return np.exp(AuAg_fit[1])*(partition_function_au1/partition_function_ag1)
 
-    plt.plot(AuAg_graph[:, 0], AuAg_graph[:, 1], "x")
-    plt.plot(AuAg_graph[:, 0], AuAg_graph[:, 0]*AuAg_fit[0]+AuAg_fit[1])
-    plt.xlabel('Difference of upper energy levels (cm-1)')
-    plt.ylabel('log of line intensity ratios (a.u.)')
-    plt.title('Saha-Boltzmann line-pair plot for Au I and Ag I lines')
-    plt.figure()
 
-    print(
-        f"The temperature is: {TAuAg}, and the total Au-to-Ag number concentration ratio is: {total_n_AuAg}")
-
-    return (TAuAg, total_n_AuAg)
+def AuAg_n_concentration(nAuAg, nAuion_atom, nAgion_atom):
+    return ((nAuion_atom+1)/(nAgion_atom+1))*nAuAg
 
 
 ### Peak tables###
@@ -184,11 +157,45 @@ AgIdata = spec_data(AgI_species, atomic_lines_ag1,
                     corrected_spectrum, integrals, peaks)
 AuAg_graph = AuAg_ratio(AuIdata, AgIdata)
 
-TAuAg, total_n_AuAg = AuAg_n_concentration(AuAg_graph)
+AuAg_fit = fit_graph(AuAg_graph)
+
+TAuAg = caculate_temperature(AuAg_fit)
+
+partition_function_ag1 = get_partition_function("Ag I", TAuAg)
+partition_function_ag2 = get_partition_function("Ag II", TAuAg)
+partition_function_au1 = get_partition_function("Au I", TAuAg)
+partition_function_au2 = get_partition_function("Au II", TAuAg)
+partition_function_ar1 = get_partition_function("Ar I", TAuAg)
+partition_function_ar2 = get_partition_function("Ar II", TAuAg)
+first_ionization_energy_ag = get_ionization_energy("Ag I")
+first_ionization_energy_au = get_ionization_energy("Au I")
+first_ionization_energy_ar = get_ionization_energy("Ar I")
+
+nAuAg = calculate_nAuAg(
+    AuAg_fit, partition_function_au1, partition_function_ag1)
+
+ne = estimate_electron_concentration(
+    TAuAg, first_ionization_energy_ar, partition_function_ar1, partition_function_ar2)
+nAgion_atom = SB(
+    ne, TAuAg, first_ionization_energy_ag)*(partition_function_ag2/partition_function_ag1)
+nAuion_atom = SB(
+    ne, TAuAg, first_ionization_energy_au)*(partition_function_au2/partition_function_au1)
+# nAgatom_ion = 1/nAgion_atom
+
+total_n_AuAg = AuAg_n_concentration(nAuAg, nAuion_atom, nAgion_atom)
 
 AuI_linepair_check = line_pair(AuI_species, AuIdata, TAuAg)
 AgI_linepair_check = line_pair(AgI_species, AgIdata, TAuAg)
 
+plt.plot(AuAg_graph[:, 0], AuAg_graph[:, 1], "x")
+plt.plot(AuAg_graph[:, 0], AuAg_graph[:, 0]*AuAg_fit[0]+AuAg_fit[1])
+plt.xlabel('Difference of upper energy levels (cm-1)')
+plt.ylabel('log of line intensity ratios (a.u.)')
+plt.title('Saha-Boltzmann line-pair plot for Au I and Ag I lines')
+plt.figure()
+
+print(
+    f"The temperature is: {TAuAg}, and the total Au-to-Ag number concentration ratio is: {total_n_AuAg}")
 
 left = peak_prominences(corrected_spectrum[:, 1], peaks, wlen=set_wlen)[
     1]  # lower integration limit of each line
@@ -214,7 +221,6 @@ plt.xlabel('Wavelength (nm)')
 plt.ylabel('Intensity (a.u.)')
 plt.title('Original spectrum and baseline')
 plt.figure()
-
 
 print(f"AuI linepair deviations: {AuI_linepair_check}")
 print(f"AgI linepair deviations: {AgI_linepair_check}")
